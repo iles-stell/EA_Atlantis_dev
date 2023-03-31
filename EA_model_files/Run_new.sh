f48599779b5ef069ae138a68f6c96c059aa40b42
iles-stell <ilaria.stollberg@gmail.com>
2023-03-31 12:21:07 +1100

    Update 1
    Added migrations csv, added columns missing from AtlantisGroups.csv due to recent updates.

--- EA_model_files/Run_new.sh
+++ EA_model_files/Run_new.sh
@@ -8,7 +8,7 @@
 ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
 ## ~           Running Atlantis JFRE        ~ ##
 ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
-atlantisMerged -i final_with_ice_input.nc 0 -o output.nc -r EA_run.prm -f EA_force.prm -p EA_physics.prm -b EA_biol.prm -h EA_harvest_nofishing.prm -s AntarcticGroupsnew2019.csv -d output_EA_biol_debug
+atlantisMerged -i final_with_ice_input.nc 0 -o output.nc -r EA_run.prm -f EA_force.prm -p EA_physics.prm -b EA_biol.prm -h EA_harvest_nofishing.prm -s AntarcticGroupsnew2019.csv -d output_EA_biol_debug -m EA_migrations.csv
 
 ## Backup of the log.txt file
 #echo "++++++++++++++++++++++++++++++++++++++++"
